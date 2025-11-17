1c4a5363797a4eef256f339ca14626bdbe591ec7
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2025-11-17 03:46:32 -0600

    Refactor AppointmentRepositoryImpl for new API methods

--- app/src/main/java/com/example/nefrovida/data/repository/AppointmentRepositoryImpl.kt
+++ app/src/main/java/com/example/nefrovida/data/repository/AppointmentRepositoryImpl.kt
@@ -1,31 +1,21 @@
 package com.example.nefrovida.data.repository
 
-import com.example.nefrovida.data.remote.api.AppointmentApi
-import com.example.nefrovida.data.mapper.toDomain
-import com.example.nefrovida.domain.model.Appointment
+import com.example.nefrovida.data.network.dto.AppointmentDetailDto
+import com.example.nefrovida.data.network.dto.AppointmentDto
 import com.example.nefrovida.domain.repository.AppointmentRepository
-import jakarta.inject.Inject
-import jakarta.inject.Singleton
-import retrofit2.Response
+import com.example.nefrovida.data.network.AppointmentApi // <-- Usando tu nombre
+import javax.inject.Inject
 
-@Singleton
-class AppointmentRepositoryImpl @Inject constructor (
-    private val api: AppointmentApi
+// Hilt sabe cómo crear 'api' gracias a tu AppModule
+class AppointmentRepositoryImpl @Inject constructor(
+    private val api: AppointmentApi 
 ) : AppointmentRepository {
-    override suspend fun getAppointmentList(): List<Appointment>{
-        val response = api.getAppointmentList()
-        return response.map { it.toDomain() }
-    }
-    override suspend fun getAppointmentListByDate(date: String): List<Appointment> {
-        val response = api.getAppointmentListByDate(date)
-        return response.map{it.toDomain()}
+
+    override suspend fun getUserAppointments(token: String): List<AppointmentDto> {
+        return api.getUserAppointments(token)
     }
 
-    override suspend fun getAppointmentById(id: Int): Appointment{
-        return api.getAppointmentById(id).toDomain()
-    }
-
-    override suspend fun cancelAppointmentById(id: Int) : Response<Unit> {
-        return api.cancelAppointment(id)
+    override suspend fun getAppointmentDetails(token: String, appointmentId: String): AppointmentDetailDto {
+        return api.getAppointmentDetails(token, appointmentId)
     }
 }
