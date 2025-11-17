1474fd9d6dba54097e2e88d69b59282dab489530
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2025-11-17 11:47:09 -0600

    Refactor LaboratoryScreen to use AgendaViewModel

--- app/src/main/java/com/example/nefrovida/presentation/screens/laboratory/LaboratoryScreen.kt
+++ app/src/main/java/com/example/nefrovida/presentation/screens/laboratory/LaboratoryScreen.kt
@@ -2,25 +2,55 @@ package com.example.nefrovida.presentation.screens.laboratory
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.nefrovida.presentation.screens.agenda.AgendaViewModel // Importa el ViewModel
 import com.example.nefrovida.presentation.screens.home.components.AgendaList
 
-@Suppress("ktlint:standard:function-naming")
 @Composable
 fun LaboratoryScreen(
     onBackClick: () -> Unit,
-    navController : NavController,
-    modifier: Modifier = Modifier
-){
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    // Hilt inyectará automáticamente el ViewModel
+    viewModel: AgendaViewModel = hiltViewModel()
+) {
+    // Observamos el estado (state) del ViewModel
+    val state by viewModel.listState.collectAsState()
+
     Box(
         modifier = modifier.fillMaxSize()
-    )  {
-        AgendaList(
-            onCardClick = { appointmentId ->
-                navController.navigate("appointment_detail/$appointmentId")
+    ) {
+        // Usamos un 'when' para reaccionar al estado
+        when {
+            // --- ESTADO DE CARGA ---
+            state.isLoading -> {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
             }
-        )*/
+            // --- ESTADO DE ERROR ---
+            state.error != null -> {
+                Text(
+                    text = "Error al cargar citas: ${state.error}",
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+            // --- ESTADO DE ÉXITO ---
+            else -> {
+                // Si todo está bien, pasamos la lista real a AgendaList
+                AgendaList(
+                    appointments = state.appointments, // <-- Datos reales
+                    onCardClick = { appointmentId ->
+                        navController.navigate("appointment_detail/$appointmentId")
+                    }
+                )
+            }
+        }
     }
 }
