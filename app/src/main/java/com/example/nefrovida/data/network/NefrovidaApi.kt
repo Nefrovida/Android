986b3ecae9d1dd4f50141bfb4bebd5a14de38ffa
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2025-11-17 03:36:01 -0600

    Add NefrovidaApi interface for appointment services

---
+++ app/src/main/java/com/example/nefrovida/data/network/NefrovidaApi.kt
@@ -0,0 +1,25 @@
+package com.example.nefrovida.data.network
+
+import com.example.nefrovida.data.network.dto.AppointmentDetailDto
+import com.example.nefrovida.data.network.dto.AppointmentDto
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+
+interface NefrovidaApi {
+
+    // Esta ruta es para el paciente
+    //
+    @GET("appointments")
+    suspend fun getUserAppointments(
+        @Header("Authorization") token: String // La ruta está protegida
+    ): List<AppointmentDto>
+
+    // Esta ruta es para los detalles
+    //
+    @GET("agenda/appointment/{id}")
+    suspend fun getAppointmentDetails(
+        @Header("Authorization") token: String, // La ruta está protegida
+        @Path("id") appointmentId: String
+    ): AppointmentDetailDto
+}
