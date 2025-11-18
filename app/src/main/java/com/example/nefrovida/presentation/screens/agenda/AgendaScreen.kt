2bf1173217483ade7e87bae43738da32ed712364
Frida Arcadia <frida.arcadialv@gmail.com>
2025-11-18 16:48:16 -0600

    chore: convert 'AppointmentCard' into generic card

--- app/src/main/java/com/example/nefrovida/presentation/screens/agenda/AgendaScreen.kt
+++ app/src/main/java/com/example/nefrovida/presentation/screens/agenda/AgendaScreen.kt
@@ -1,5 +1,6 @@
 package com.example.nefrovida.presentation.screens.agenda
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
