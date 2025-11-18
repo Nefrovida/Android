342c14406936f067c3e8830ab35d0034b6437868
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2025-11-18 16:46:06 -0600

    Add getAppointmentDetails function to repository

--- app/src/main/java/com/example/nefrovida/domain/repository/AppointmentRepository.kt
+++ app/src/main/java/com/example/nefrovida/domain/repository/AppointmentRepository.kt
@@ -10,6 +10,7 @@ interface AppointmentRepository {
     // --- Patient Section (feature-13) ---
     suspend fun getUserAppointments(token: String): List<AppointmentDto>
     suspend fun getPatientAppointmentDetails(token: String, appointmentId: String): AppointmentDetailDto
+    suspend fun getAppointmentDetails(token: String, appointmentId: String): AppointmentDetailDto
 
     // --- Secretary Section (feature-16) ---
     suspend fun getAppointmentList(): List<Appointment>
