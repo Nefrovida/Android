60afaea8c5a4f5c19b9abd6ce3e6bf363b34eff0
Ricardo Calzada <a01707647@tec.mx>
2025-12-02 16:55:50 -0600

    feat: Profile Repository created

--- app/src/main/java/com/example/nefrovida/domain/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/nefrovida/domain/repository/ProfileRepository.kt
@@ -1,2 +1,14 @@
 package com.example.nefrovida.domain.repository
 
+import com.example.nefrovida.data.remote.dto.ChangePasswordDto
+import com.example.nefrovida.data.remote.dto.UpdateProfileDto
+import com.example.nefrovida.domain.model.UserProfile
+import com.example.nefrovida.utils.Resource
+
+interface ProfileRepository {
+    suspend fun getMyProfile(): Resource<UserProfile>
+
+    suspend fun updateMyProfile(updateProfileDto: UpdateProfileDto): Resource<UserProfile>
+
+    suspend fun changePassword(changePasswordDto: ChangePasswordDto): Resource<Unit>
+}
